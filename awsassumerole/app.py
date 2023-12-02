9dbf46594a00debf71a77dda12c5160e671b4a0e
David <c0d3@gpobox.net>
2023-12-02 19:54:39 +1100

    Support aws-assume-role specifying a role arn

--- awsassumerole/app.py
+++ awsassumerole/app.py
@@ -8,13 +8,16 @@ def main():
     parser = ArgumentParser(description='AWS assume role')
     parser.add_argument('--profile', help='AWS profile', default=os.getenv('AWS_PROFILE', 'default'))
     parser.add_argument('--region', help='AWS region', default=os.getenv('AWS_REGION', 'ap-southeast-2'))
-    parser.add_argument('--role', help='AWS role to assume', required=True)
+    parser.add_argument('--role', help='AWS role name or role arn to assume', required=True)
     parser.add_argument('--name', help='AWS role session name', default='assumed')
     args = parser.parse_args()
     session = boto3.Session(profile_name=args.profile, region_name=args.region)
     iam_client = session.client('iam')
-    response = iam_client.get_role(RoleName=args.role)
-    role_arn = response['Role']['Arn']
+    if args.role.startswith('arn:aws:'):
+        role_arn = args.role
+    else:
+        response = iam_client.get_role(RoleName=args.role)
+        role_arn = response['Role']['Arn']
     sts_client = session.client('sts')
     response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=args.name)
     access_key_id = response['Credentials']['AccessKeyId']
